cc425512fd15b476ae32e3f13e56ae197d13ae3c
Yossi Mosbacher <joe.mosbacher@gmail.com>
2023-02-19 13:10:49 +0200

    move data files

--- xefab/tasks/mc_chain_new/files.py
+++ xefab/tasks/mc_chain_new/files.py
@@ -5,7 +5,7 @@ from xefab.file_access import TemplateFile
 
 
 class RunSim(TemplateFile):
-    __TEMPLATE__ = files("data.mc_chain").joinpath("run_sim.sh").read_text()
+    __TEMPLATE__ = files("xefab.data.mc_chain").joinpath("run_sim.sh").read_text()
 
     filename: str = "run_sim.sh"
     simulation_name: str
@@ -22,14 +22,14 @@ class RunSim(TemplateFile):
 
 class Merge(TemplateFile):
     
-    __TEMPLATE__ = files("data.mc_chain").joinpath("merge.sh").read_text()
+    __TEMPLATE__ = files("xefab.data.mc_chain").joinpath("merge.sh")
 
     filename: str = "merge.sh"
     run_id: str
     inputs: List[str]
 
 class RunWfSim(TemplateFile):
-    __TEMPLATE__ = files("data.mc_chain").joinpath("run_wfsim-wrapper.sh").read_text()
+    __TEMPLATE__ = files("xefab.data.mc_chain").joinpath("run_wfsim-wrapper.sh")
 
     filename = "run_wfsim.sh"
     input_tarball: str
@@ -49,7 +49,7 @@ class RunWfSim(TemplateFile):
 
 
 class MergeWfSim(TemplateFile):
-    __TEMPLATE__ = files("data.mc_chain").joinpath("merge_wfsim-wrapper.sh").read_text()
+    __TEMPLATE__ = files("xefab.data.mc_chain").joinpath("merge_wfsim-wrapper.sh")
 
     filename: str = "merge_wfsim.sh"
     batch_number: int = 0
@@ -62,7 +62,7 @@ class MergeWfSim(TemplateFile):
 
 
 class UploadWrapper(TemplateFile):
-    __TEMPLATE__ = files("data.mc_chain").joinpath("upload-wrapper.sh").read_text()
+    __TEMPLATE__ = files("xefab.data.mc_chain").joinpath("upload-wrapper.sh")
 
     filename = "upload_wrapper.sh"
     config_file: str
